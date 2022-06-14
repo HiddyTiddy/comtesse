a3329faeb06d678918548673047d4ab4ad414fd5
HiddyTiddy <hiddy.tiddey@gmail.com>
2022-06-14 11:13:46 +0200

    test for is_connected

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -9,3 +9,37 @@ where
         todo!()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::weighted::Weighted;
+
+    #[test]
+    fn is_connected() {
+        let mut graph: Weighted<_, f32> = ('a'..='f').collect();
+        graph.construct_edges_from(|&from, &to| match (from, to) {
+            ('a', 'b') => Some(9.0),
+            ('a', 'd') => Some(8.0),
+            ('b', 'c') => Some(1.0),
+            ('b', 'e') => Some(3.0),
+            ('c', 'e') => Some(1.0),
+            ('c', 'd') => Some(5.0),
+            ('d', 'f') => Some(8.0),
+            ('e', 'f') => Some(6.0),
+            _ => None,
+        });
+
+        assert!(graph.is_connected());
+
+        graph.remove_edge(
+            graph.get_vertex('a').unwrap(),
+            graph.get_vertex('b').unwrap(),
+        );
+        graph.remove_edge(
+            graph.get_vertex('a').unwrap(),
+            graph.get_vertex('d').unwrap(),
+        );
+
+        assert!(!graph.is_connected());
+    }
+}
